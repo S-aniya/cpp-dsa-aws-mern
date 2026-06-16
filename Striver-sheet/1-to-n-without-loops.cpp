f8987a1be98015113969811367a6e9049081d462
SANIYA GUPTA <160046544+S-aniya@users.noreply.github.com>
2026-06-16 14:56:21 +0000

    recusrion basics

---
+++ Striver-sheet/1-to-n-without-loops.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void printTillN(int n) {
+        // code here
+        
+        if(n==0)
+            return;
+        else
+            printTillN(n-1);
+            cout<<n<<" ";
+    }
+};
