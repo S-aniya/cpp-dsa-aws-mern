3df448fb8053b9cd10811bd2613c429bbc5de39e
SANIYA GUPTA <160046544+S-aniya@users.noreply.github.com>
2026-06-17 13:03:44 +0000

    reverse array using recursion

---
+++ Striver-sheet/7.reverse-an-array-using-recursion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code 
+        int left=0;
+        int right =arr.size()-1;
+        while(left<right)
+        {
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+        }
+        return;
+        
+    }
+};
+
+class Solution {
+  public:
+    void helper(vector<int> &arr, int left,int right)
+    {
+        if(left>=right)
+        {
+            return;
+        }
+        else
+        {
+            swap(arr[left],arr[right]);
+            helper(arr,left+1,right-1);
+        }
+    }
+    void reverseArray(vector<int> &arr) {
+        // code 
+        helper(arr,0,arr.size()-1);
+        
+    }
+};
